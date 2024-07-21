83fd4e92c2761e061128992e2c1816c21dfb077a
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-07-21 14:53:20 +0200

    changes

--- src/app/components/home/Home.tsx
+++ src/app/components/home/Home.tsx
@@ -1,5 +1,13 @@
+'use client';
+
 import React, { useEffect, useState, useRef } from 'react';
 import * as prismic from '@prismicio/client';
+
+import { useStore } from 'zustand';
+
+// Store Import
+
+import { projectStore } from '../../../../stores/projectStore';
 
 export default function Home({
   styles,
@@ -9,6 +17,8 @@ export default function Home({
   projects: any;
   home: any;
 }) {
+  const isHovered = useStore(projectStore).isHovered;
+  const isClicked = useStore(projectStore).isClicked;
   return (
     <>
       <section className={`${styles.ContentContainer} ${styles.ProjectList}`}>
@@ -27,7 +37,17 @@ export default function Home({
         </div>
       </section>
       <section className={styles.ContentContainer}>
-        <div className={styles.Images}></div>
+        <div className={styles.Images}>
+          {!isHovered && !isClicked ? (
+            <div className={styles.MainImageContainer}>
+              <img
+                src="https://images.prismic.io/pierogood/Zpz8BR5LeNNTxU88_Bildschirmfoto2022-09-02um14.11.30.png?auto=format,compress"
+                alt="Fischhaut reingezoomt"
+                className={styles.MainImage}
+              />
+            </div>
+          ) : null}
+        </div>
       </section>
     </>
   );
--- stores/projectStore.ts
+++ stores/projectStore.ts
@@ -1,1 +1,15 @@
 import { create } from 'zustand';
+
+interface ProjectStore {
+  isHovered: boolean | string;
+  setIsHovered: (isHovered: boolean) => void;
+  isClicked: boolean | string;
+  setIsClicked: (isClicked: boolean) => void;
+}
+
+export const projectStore = create<ProjectStore>((set) => ({
+  isHovered: false,
+  setIsHovered: (isHovered: boolean | string) => set({ isHovered }),
+  isClicked: false,
+  setIsClicked: (isClicked: boolean | string) => set({ isClicked }),
+}));
